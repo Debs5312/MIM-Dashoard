77e3f6ba724f5e3e5ff56460395c5ff218e8c9bf
Debs5312 <debojyotisaha5@gmail.com>
2025-08-02 00:28:34 +0530

    Update logger logic to include endpoint

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   const now = new Date();
   const timeString = now.toTimeString().split(' ')[0]; // hh:mm:ss
   const clientIp = req.ip || req.connection.remoteAddress || 'unknown';
-  const logEntry = `date-{${timeString}} - ${clientIp} - ${req.method}\n`;
+  const logEntry = `date-{${timeString}} - ${clientIp} - ${req.method} - ${req.originalUrl}\n`;
   const logFilePath = path.join(__dirname, 'api.log');
   fs.appendFile(logFilePath, logEntry, (err) => {
     if (err) {
